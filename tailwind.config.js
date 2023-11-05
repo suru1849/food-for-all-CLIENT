c4309fe1113fc592a16c5d32bfc35f5ebf139773
suru1849 <suranjan1849@gmail.com>
2023-11-05 22:28:01 +0600

    Basic setup with 'react-router','TailwindCss','DaisyUi','FlowBite'

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // diasyUi
+    "./src/**/*.{js,ts,jsx,tsx}", // diasyUi
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}", //flowbite
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui"), require("flowbite/plugin")],
+};
