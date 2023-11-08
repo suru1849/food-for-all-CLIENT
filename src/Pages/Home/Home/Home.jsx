a8189f903946b02bea8e6d288f4d5b24706ae2eb
suru1849 <suranjan1849@gmail.com>
2023-11-09 02:20:26 +0600

    Our Services added in home page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import AvailableFoodCard from "../../AvailableFoods/AvailableFoodCard";
 import { Link } from "react-router-dom";
+import img1 from "../../../assets/image1.png";
+import img2 from "../../../assets/image2.png";
+import img3 from "../../../assets/image5.png";
 
 const Home = () => {
   const [foods, setFoods] = useState([]);
@@ -31,7 +34,7 @@ const Home = () => {
         </div>
         {/* featured food */}
         <div>
-          <h1 className="text-center font-kenia font-medium text-4xl mt-28">
+          <h1 className="text-center font-kenia  text-4xl mt-28">
             Featured Foods
           </h1>
           <div className="grid gap-5 grid-cols-1 lg:grid-cols-2 my-10">
@@ -43,6 +46,29 @@ const Home = () => {
             <Link to="/available-foods" className="btn btn-warning">
               Show All
             </Link>
+          </div>
+        </div>
+        {/* our servuces */}
+        <div>
+          <h1 className="text-center font-kenia  text-4xl mt-28">
+            Our Services
+          </h1>
+          <div className="flex flex-col md:flex-row gap-5 my-16">
+            {/* one */}
+            <div className="flex flex-col justify-center items-center gap-3 border-2 md:w-1/2 rounded-xl p-2 shadow-lg font-bold text-lg">
+              <img className="w-56" src={img1} />
+              <p>Best Service Promise</p>
+            </div>
+            {/* two */}
+            <div className="flex flex-col justify-center items-center gap-3 border-2 md:w-1/2 rounded-xl p-2 shadow-lg font-bold text-lg">
+              <img className="w-56" src={img2} />
+              <p>Fullfill Your Needs</p>
+            </div>
+            {/* Three */}
+            <div className="flex flex-col justify-center items-center gap-3 border-2 md:w-1/2 rounded-xl p-2 shadow-lg font-bold text-lg">
+              <img className="w-56" src={img3} />
+              <p>24/7 hr Customer Care</p>
+            </div>
           </div>
         </div>
       </div>
